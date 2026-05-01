eee266b172a2acce5a9c0b371036f24b2e397195
Aidan B <aidanb1049@gmail.com>
2026-05-01 12:46:29 -0700

    bugfix - realsense

--- src/drivers/sensors/RealsenseCamera.py
+++ src/drivers/sensors/RealsenseCamera.py
@@ -131,7 +131,7 @@ class RealsenseCam(DriverBase):
 
             # skip the first 5 frames, allow for auto-exposure
             for x in range(5):
-                pipe.try_wait_for_frames()
+                self.realsense_pipeline.try_wait_for_frames()
             
             # Attempt to retrive the most recent frame from the realsense camera
             capSuccsess, frames = self.realsense_pipeline.try_wait_for_frames()
