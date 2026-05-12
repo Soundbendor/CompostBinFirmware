57395f417d931327bc023688aa34e77db2f5dd3e
Aidan B <aidanb1049@gmail.com>
2026-05-12 14:20:04 -0700

    staging

--- src/drivers/sensors/BME688.py
+++ src/drivers/sensors/BME688.py
@@ -1,18 +1,17 @@
-""" "
-Will Richards, Oregon State University, 2023
-
+"""
 Abstraction layer for the BME688 gas sensor
 """
 
 import logging
-from time import time, sleep
-import os
+from time import time
+from pathlib import Path
 
 from bme68x import BME68X
+import bme68xConstants as bme_cnst
 import bsecConstants as bsec
 
 from drivers.DriverBase import DriverBase
-from multiprocessing import Event, Value
+from multiprocessing import Value
 
 
 class BME688(DriverBase):
@@ -28,13 +27,14 @@ class BME688(DriverBase):
         self.i2c_address = i2c_address
         self.sensor = None
         self.failedToInit = False
+        self.sample_rate = bsec.BSEC_SAMPLE_RATE_LP
+        self.heater_status = bme_cnst.BME68X_ENABLE
+        self.parallel_mode = bme_cnst.BME68X_PARALLEL_MODE
+        self.temp_prof = [320, 100, 100, 100, 200, 200, 200, 320, 320, 320]
+        self.dur_prof = [5, 2, 10, 30, 5, 5, 5, 5, 5, 5]
 
         # Set this process to loop once a second
         self.setLoopTime(1)
-
-        # When the device is restarted we want to clear the last savedState
-        if os.path.exists("savedState.dat"):
-            os.remove("savedState.dat")
 
         self.startTime = time()
 
@@ -46,8 +46,18 @@ class BME688(DriverBase):
         try:
             # i2c_bus = 1 is standard for Raspberry Pi main I2C bus
             self.sensor = BME68X(self.i2c_address, 1)
-            self.sensor.set_heatr_conf(1, 320, 100, 1)
-            self.sensor.set_sample_rate(bsec.BSEC_SAMPLE_RATE_LP)
+            self.sensor.set_heatr_conf(
+                self.heater_status, self.temp_prof, self.dur_prof, self.parallel_mode
+            )
+            # read config file
+            state_int = self._readState(self.calibration_file)
+            # If calibration file does not exist:
+            if not state_int:
+                # TODO: Fork new job to run BME688 sensor burn-in
+                pass
+            else:
+                self.sensor.set_bsec_state(state_int)
+            self.sensor.set_sample_rate(self.sample_rate)
 
             logging.info("Initialization complete!")
             self.initialized = True
@@ -120,3 +130,24 @@ class BME688(DriverBase):
 
     def kill(self):
         self.sensor = None
+
+    """
+    Read the calibration curve for the BME688 sensor
+    This calibration curve should be generated during bin calibration
+    """
+
+    def _readState(self, state_file_name) -> list[int] | None:
+        state_path = str(
+            Path(__file__).resolve().parent.joinpath("conf", state_file_name)
+        )
+        if state_path.is_file():
+            state_file = open(state_path, "r")
+            # strip the brackets [.....]
+            state_str = state_file.read()[1:-1]
+            # split on delimiter ,
+            state_list = state_str.split(",")
+            state_int = [int(x) for x in state_list]
+            return state_int
+        else:
+            # Failed to load calibration curve
+            return None
